bbe57ebc1a0d1a09393f8d4c87b5cd539590331f
George <gc14768@my.bristol.ac.uk>
2017-10-21 17:43:35 +0100

    Created instruction class to hold information for each instruction

--- src/processor.cpp
+++ src/processor.cpp
@@ -1,6 +1,20 @@
 #include <stdio.h>
 #include "register_file.h"
 #include "alu.h"
+#include <iostream>
+
+class Instruction {
+	public:
+        std::string assembly;
+        int opcode;
+        int operands[3];
+    	Instruction(std::string a, int b, int c, int d, int e) : assembly(a), opcode(b) {
+    		operands[0] = c;
+    		operands[1] = d;
+    		operands[2] = e;
+    	}
+};
+
 
 class Processor {
     int pc;
@@ -26,6 +40,11 @@ class Processor {
 
 
 int main(void) {
+	Instruction i1("ADDI R1 R1 R2", 0, 1, 1, 2);
+	Instruction i2("ADDI R2 R2 R3", 0, 2, 2, 3);
+	Instruction i3("ADD R0 R1 R2", 1, 0, 1, 2);
+	Instruction i4("SUB R3 R1 R0", 2, 3, 1, 0);
+	Instruction instructions[] = {i1, i2, i3, i4};
 	Processor processor;
 	processor.info();
 	return 0;
