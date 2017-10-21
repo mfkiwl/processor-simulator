51cdcd6a00f0c3e1fa757fbdeb6c61118c81de81
George <gc14768@my.bristol.ac.uk>
2017-10-21 18:10:26 +0100

    Added vector of instructions member to the processor

--- src/processor.cpp
+++ src/processor.cpp
@@ -2,6 +2,7 @@
 #include "register_file.h"
 #include "alu.h"
 #include <iostream>
+#include <vector>
 
 class Instruction {
 	public:
@@ -17,35 +18,34 @@ class Instruction {
 
 
 class Processor {
+	std::vector<Instruction> instructions;
     int pc;
     int noOfInstructionsExecuted;
     RegisterFile registerFile;
     ALU alu;
     public:
     	//Classes needed to be initialised in the uniform initialiser list
-        Processor() : pc(0), noOfInstructionsExecuted(0), registerFile(8), alu(registerFile) {}
+        Processor(std::vector<Instruction> a) : instructions(a), pc(0), noOfInstructionsExecuted(0), registerFile(8), alu(registerFile) {}
 
-        void info() {
-        	registerFile.printRegisterValues();
-            alu.ADDI(1,1,2);
-            registerFile.printRegisterValues();
-            alu.ADDI(2,2,3);
-            registerFile.printRegisterValues();
-            alu.ADD(0,1,2);
-            registerFile.printRegisterValues();
-            alu.SUB(3,1,0);
-            registerFile.printRegisterValues();
+        void printInstructions() {
+        	for(int i = 0; i < instructions.size(); i++) {
+        		std::cout << instructions[i].assembly << std::endl;
+        	}
+        }
+
+        void printpc() {
+        	printf("%d\n", pc);
         }
 };
 
 
 int main(void) {
-	Instruction i1("ADDI R1 R1 R2", 0, 1, 1, 2);
-	Instruction i2("ADDI R2 R2 R3", 0, 2, 2, 3);
-	Instruction i3("ADD R0 R1 R2", 1, 0, 1, 2);
-	Instruction i4("SUB R3 R1 R0", 2, 3, 1, 0);
-	Instruction instructions[] = {i1, i2, i3, i4};
-	Processor processor;
-	processor.info();
+	std::vector<Instruction> instructions;
+	instructions.push_back(Instruction("ADDI R1 R1 R2", 0, 1, 1, 2));
+	instructions.push_back(Instruction("ADDI R2 R2 R3", 0, 2, 2, 3));
+	instructions.push_back(Instruction("ADD R0 R1 R2", 1, 0, 1, 2));
+	instructions.push_back(Instruction("SUB R3 R1 R0", 2, 3, 1, 0));
+	Processor processor(instructions);
+	processor.printInstructions();
 	return 0;
 }
