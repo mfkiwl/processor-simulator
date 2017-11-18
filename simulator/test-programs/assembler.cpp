663368e251c33be61a395660b2f50d119826c10e
George <gc14768@my.bristol.ac.uk>
2017-11-18 22:45:26 +0000

    assembler takes the name of the assembly file as a command line argument

--- simulator/test-programs/assembler.cpp
+++ simulator/test-programs/assembler.cpp
@@ -40,33 +40,63 @@ void printOperands(string operands, ofstream *newfile) {
 	*newfile << operands;
 }
 
-int main(void) {
+string createOutputFileName(string inputFileName) {
+    string name = inputFileName.substr(0, inputFileName.find('.'));
+    string extension = ".mac";
+    string outputFileName = name + extension;
+    return outputFileName;
+}
+
+int main(int argc, char *argv[]) {
+
+	//make sure an argument is given
+	if(argc != 2) {
+		cout << "incorrect arguments\n";
+		return 1;
+	}
+
+    //get the input file name
+    string inputFileName(argv[1]);
+    cout << "Input assembly file: " + inputFileName + '\n';
+
+    //create the new file name
+	string outputFileName = createOutputFileName(inputFileName);
+    cout << "Output machine file: " + outputFileName + "\n";
+
+	//the input and output files
+	ifstream inputFile(inputFileName.c_str());
+	ofstream outputFile(outputFileName.c_str());
+
+	//begin writing machine to the output file
 	string line;
-	ifstream myfile("memory.ass");
-	ofstream newfile("memory.mac");
-	if(myfile.is_open() && newfile.is_open()) {
-		while (getline(myfile,line)) {
+	if(inputFile.is_open() && outputFile.is_open()) {
+		while (getline(inputFile,line)) {
             
             //get the opcode
 			string delimiter = " ";
 			int delimiterPos = line.find(delimiter);
 			string opstr = line.substr(0, delimiterPos);
-			printOpcode(opstr, &newfile);
+			printOpcode(opstr, &outputFile);
 
 			//get the operands
 			string operands = line.substr(delimiterPos + 1, line.size());
-			printOperands(operands, &newfile);
+			printOperands(operands, &outputFile);
 
             //finish line
-			newfile << '\n';
+			outputFile << '\n';
 
 		}
-		myfile.close();
-		newfile.close();
-		cout << "new file created.\n";
+
+		//close the files
+		inputFile.close();
+		outputFile.close();
+
+		//print success message
+		cout << "assembler was successful.\n";
 	}
 	else {
-		cout << "Unable to open file";
+		//print failure message
+		cout << "Unable to open file\n";
 	}
 	return 0;
 }
