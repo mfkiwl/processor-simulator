1960e58ce9297d84d21d82972615fa6ed1a6cf32
George <gc14768@my.bristol.ac.uk>
2017-11-19 17:24:41 +0000

    Moved the code in processor.cpp that reads the machine code file to the get_instructions.h header file

--- simulator/src/processor.cpp
+++ simulator/src/processor.cpp
@@ -6,6 +6,7 @@
 #include <string>
 
 #include "instruction.h"
+#include "get_instructions.h"
 #include "register_file.h"
 #include "memory.h"
 #include "alu.h"
@@ -104,72 +105,6 @@ class Processor {
         }
 };
 
-int getNumOfInstructions(std::string inputFileName) {
-    std::ifstream inputFile(inputFileName.c_str());
-    if(inputFile.is_open()) {
-        int i = 0;
-        std::string line;
-        while(getline(inputFile,line)) {
-            i++;
-        }
-        return i;
-    }
-    else {
-        printf("Failed to read file.\n");
-        return -1;
-    }
-}
-
-Instruction *getInstructions(std::string inputFileName, int numOfInstructions) {
-    //open file
-    std::ifstream inputFile(inputFileName.c_str());
-    //if file is open then get the instruction information from each line
-    if(inputFile.is_open()) {
-        printf("Reading instructions from %s\n", inputFileName.c_str());
-        //allocating memory to array to store the instructions
-        Instruction *instructions = (Instruction*) malloc(sizeof(Instruction) * numOfInstructions);
-
-        //interating through each instruction
-        std::string line;
-        for(int i = 0; i < numOfInstructions; i++) {
-            getline(inputFile,line);
-
-            //get the opcode
-            int pos = line.find(" ");
-            std::string opcodestr = line.substr(0,pos);
-            line = line.substr(pos + 1, line.size());
-            int opcode = atoi(opcodestr.c_str());
-            instructions[i].opcode = opcode;
-            
-            //get the operands
-            pos = line.find(" ");
-            int j = 0;
-            while(pos != std::string::npos) {
-                //get operand
-                std::string operandstr = line.substr(0, pos);
-                int operand = atoi(operandstr.c_str());
-                instructions[i].operands[j] = operand;
-
-                //update values
-                line = line.substr(pos + 1, line.size());
-                pos = line.find(" ");
-                j++;
-            }
-            //get the final operand
-            std::string operandstr = line.substr(0, line.size());
-            int operand = atoi(operandstr.c_str());
-            instructions[i].operands[j] = operand;
-        }
-        //return the instructions array
-        return instructions;
-    }
-    else {
-        printf("Failed to read file.\n");
-        //if the file has not been opened then return a null pointer
-        return NULL;
-    }
-}
-
 int main(int argc, char *argv[]) {
     //If command line arguments are incorrect then stop program
     if(argc != 2) {
@@ -177,7 +112,7 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    //extract the instructions from the machine code file
+    //get the instructions from the machine code file
     std::string inputFileName(argv[1]);
     int numOfInstructions = getNumOfInstructions(inputFileName);
     Instruction *instructions = NULL;
