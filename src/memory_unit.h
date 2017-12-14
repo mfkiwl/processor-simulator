d1e1303215c99feb2d110bb0c243fe197bd2125d
George <gc14768@my.bristol.ac.uk>
2017-12-14 12:57:12 +0000

    Readded the memory read write buffer stuff

--- src/memory_unit.h
+++ src/memory_unit.h
@@ -13,25 +13,57 @@ class MemoryUnit {
     //for debugging purposes
     Instruction DEBUG_Instruction;
 
+    //read buffer
+    int** readBuffer;
+    int readBufferSize;
+    int readBufferStart;
+    int readBufferEnd;
+    int readBufferSteps;
+
+    //write buffer
+    int** writeBuffer;
+    int writeBufferSize;
+    int writeBufferStart;
+    int writeBufferEnd;
+    int writeBufferSteps;
+
     public:
         MemoryUnit(Memory* memory, RegisterFile* registerFile, int* noOfInstructionsExecuted) : 
             memory(memory),
             registerFile(registerFile),
             noOfInstructionsExecuted(noOfInstructionsExecuted),
-            opcode(0)
+            opcode(0),
+            writeBufferSize(100),
+            writeBufferStart(0),
+            writeBufferEnd(0),
+            writeBufferSteps(5),
+            readBufferSize(100),
+            readBufferStart(0),
+            readBufferEnd(0),
+            readBufferSteps(5)
         {
+            //initially set all operands to zero
             for(int i = 0; i < 3; i++) {
                 operands[i] = 0;
+            }
+            //dynamically allocated a 2d array to the read and write buffer
+            writeBuffer = new int*[writeBufferSize];
+            readBuffer = new int*[readBufferSize];
+            for(int i = 0; i < writeBufferSize; i++) {
+                writeBuffer[i] = new int[3];
+                readBuffer[i] = new int[3];
+            }
+            for(int i = 0; i < writeBufferSize; i++) {
+                for(int j = 0; j < 3; j++) {
+                    writeBuffer[i][j] = 0;
+                    readBuffer[i][j] = 0;
+                }
             }
         }
 
         void execute() {
             if(opcode != 0) {
-/*
-                printf("Memory unit:\n");
-                printf("opcode: %d\n", opcode);
-                printf("operands: %d %d %d\n", operands[0], operands[1], operands[2]);
-*/
+
                 int address;
                 int value;
 
@@ -59,11 +91,7 @@ class MemoryUnit {
 
                 //increment the number of instructions executed
                 (*noOfInstructionsExecuted) += 1;
-/*
-                printf("Memory unit:\n");
-                printf("opcode: %d\n", opcode);
-                printf("operands: %d %d %d\n", operands[0], operands[1], operands[2]);
-*/
+
                 //print the instruction that has been executed
                 cout << "Executed instruction: ";
                 printInstruction(DEBUG_Instruction);
@@ -75,6 +103,62 @@ class MemoryUnit {
                 }
                 DEBUG_Instruction = (Instruction) {0,0,0,0};
     	    }
+
+            //perform the write buffer operations
+            writeBufferStep();
+            writeIfReady();
+        }
+
+        int waitingForMemory() {
+            if(writeBufferStart != writeBufferEnd) {
+                return 1;
+            }
+            else {
+                return 0;
+            }
+        }
+
+        void addToWriteBuffer(int address, int value) {
+            if(writeBufferStart > 0) {
+                writeBufferStart -= 1;
+                writeBuffer[writeBufferStart][0] = address;
+                writeBuffer[writeBufferStart][1] = value;
+                writeBuffer[writeBufferStart][2] = 0;
+            }
+            else if(writeBufferEnd < writeBufferSize - 1) {
+                writeBuffer[writeBufferEnd][0] = address;
+                writeBuffer[writeBufferEnd][1] = value;
+                writeBuffer[writeBufferEnd][2] = 0;
+                writeBufferEnd += 1;
+            }
+        }
+
+        void writeBufferStep() {
+            for(int i = writeBufferStart; i < writeBufferEnd; i++) {
+                writeBuffer[i][2] += 1;
+            }
+        }
+
+        void writeIfReady() {
+            for(int i = writeBufferStart; i < writeBufferEnd; i++) {
+                //for each entry check if it is ready to write
+                if(writeBuffer[i][2] == writeBufferSteps) {
+                    //write to memory
+                    int address = writeBuffer[i][0];
+                    int value = writeBuffer[i][1];
+                    memory->storeInMemory(address, value);
+                    //reset write buffer entry
+                    writeBuffer[i][0] = 0;
+                    writeBuffer[i][1] = 0;
+                    writeBuffer[i][2] = 0;
+                    if(i == writeBufferStart) {
+                        writeBufferStart += 1;
+                    }
+                    else if(i == writeBufferEnd - 1) {
+                        writeBufferEnd -= 1;
+                    }
+                }
+            }
         }
 
         void setOpcode(int x) {
